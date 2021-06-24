0b522eadf715e9a3e62808fede6713ca4785945c
kaveenakash <kaveenakashya@gmail.com>
2021-06-24 11:27:49 +0530

    Backend base project

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+
+
+const authRoutes = require('./routes/auth-routes');
+const HttpError = require('./models/http-error');
+
+
+app.use(express.json())
+app.use('/api/auth',authRoutes)
+
+
+app.use((req,res,next) =>{
+    const error = new HttpError('could not find this route',404)
+    throw error;
+})
+
+app.use((error,req,res,next) =>{
+    if(res.headerSent){
+        return next(error)
+    }
+    res.status(error.code | 500)
+    res.json({message:error.message || 'An unknown error occured'})
+})
+
+
+
+mongoose
+.connect('mongodb+srv://ICAF_admin:ICAF_123@icaf.w04n0.mongodb.net/ICAF?retryWrites=true&w=majority',{useUnifiedTopology:true,useCreateIndex:true,useNewUrlParser:true})
+.then(() =>{
+    console.log('Database Estabilished')
+    app.listen(9090,() =>{
+        console.log('Server Started ',8000)
+    })
+})
+.catch(err =>{
+    console.log(err) 
+})
